ebc40c08c8477103e67adf0b96ad6174129af66e
Luis <luisdaniel2707@live.com>
2017-08-06 00:03:35 -0600

    First commit

---
+++ cmake-build-debug/CMakeFiles/Remote_Memory.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Remote_Memory.dir/main.cpp.o"
+  "Remote_Memory.pdb"
+  "Remote_Memory"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Remote_Memory.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
